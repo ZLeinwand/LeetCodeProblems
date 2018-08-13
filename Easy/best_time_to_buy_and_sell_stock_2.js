2f6e257b5f8dcd92b41600b9f9063a721e2ecdee
Zack Leinwand <aleinwand2020@gmail.com>
2018-08-13 15:30:14 -0400

    add max profit 2 in js

---
+++ Easy/best_time_to_buy_and_sell_stock_2.js
@@ -0,0 +1,39 @@
+/**
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+* @param {number[]} prices
+* @return {number}
+*/
+const maxProfit = (prices) => {
+  let max = 0
+  let currentLow = prices[0]
+  let currentHigh = prices[0]
+
+  for (let i = 0; i < prices.length; i++){
+    if (i == 0){
+      continue
+    }
+
+    let prevPrice = prices[i - 1]
+
+    if (prices[i] < prevPrice){
+      max += currentHigh - currentLow
+      currentLow = prices[i]
+      currentHigh = prices[i]
+    }else if (prices[i] > prevPrice){
+      currentHigh = prices[i]
+    }
+
+    if (i == prices.length - 1){
+    max += currentHigh - currentLow
+    }
+  }
+
+  return max
+}
+
+console.log(maxProfit([7,1,5,3,6,4]) == 7)
+console.log(maxProfit([1,2,3,4,5]) == 4)
+console.log(maxProfit([7,6,4,3,1]) == 0)
