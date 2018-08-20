bdcfcee57c1b476d3d2ed8173cdb98f0be2dcedc
Zack Leinwand <aleinwand2020@gmail.com>
2018-08-20 17:47:38 -0400

    add contains dups in ruby

---
+++ contains_dups.rb
@@ -0,0 +1,29 @@
+# https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/578/
+#
+# Given an array of integers, find if the array contains any duplicates.
+#
+# Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+#
+# Example 1:
+#
+# Input: [1,2,3,1]
+# Output: true
+# Example 2:
+#
+# Input: [1,2,3,4]
+# Output: false
+# Example 3:
+#
+# Input: [1,1,1,3,3,4,3,2,4,2]
+# Output: true
+
+def contains_duplicate(nums)
+    dup_hash = Hash.new(false)
+
+    nums.each do |num|
+        return true if dup_hash[num]
+        dup_hash[num] = true
+    end
+
+    false
+end
